5d605679f49bc54621b5a85382802132b1c59156
Prakhar Chanana <81297012+prakhar-chanana@users.noreply.github.com>
2021-06-25 21:23:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+function changecolor(){
+    var r = Math.floor(Math.random()* 256);
+    var g = Math.floor(Math.random()* 256);
+    var b = Math.floor(Math.random()* 256);
+    var mycolor = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('piano').style.background = mycolor;
+}
+
+function play_a3(){
+    var audio_a3 = document.getElementById('note_a3');
+    audio_a3.play();
+}
+
+function play_a_3(){
+    var audio_a_3 = document.getElementById('note_a_3');
+    audio_a_3.play();
+}
+
+function play_a4(){
+    var audio_a4 = document.getElementById('note_a4');
+    audio_a4.play();
+}
+
+function play_a_4(){
+    var audio_a_4 = document.getElementById('note_a_4');
+    audio_a_4.play();
+}
+
+function play_b3(){
+    var audio_b3 = document.getElementById('note_b3');
+    audio_b3.play();
+}
+
+function play_b4(){
+    var audio_b4 = document.getElementById('note_b4');
+    audio_b4.play();
+}
+
+function play_c3(){
+    var audio_c3 = document.getElementById('note_c3');
+    audio_c3.play();
+}
+
+function play_c_3(){
+    var audio_c_3 = document.getElementById('note_c_3');
+    audio_c_3.play();
+}
+
+function play_c4(){
+    var audio_c4 = document.getElementById('note_c4');
+    audio_c4.play();
+}
+
+function play_c_4(){
+    var audio_c_4 = document.getElementById('note_c_4')
+    audio_c_4.play();
+}
+
+function play_d3(){
+    var audio_d3 = document.getElementById('note_d3');
+    audio_d3.play();
+}
+
+function play_d_3(){
+    var audio_d_3 = document.getElementById('note_d_3');
+    audio_d_3.play();
+}
+
+function play_d4(){
+    var audio_d4 = document.getElementById('note_d4');
+    audio_d4.play();
+}
+
+function play_d_4(){
+    var audio_d_4 = document.getElementById('note_d_4');
+    audio_d_4.play();
+}
+
+function play_e3(){
+    var audio_e3 = document.getElementById('note_e3');
+    audio_e3.play();
+}
+
+function play_e4(){
+    var audio_e4 = document.getElementById('note_e4');
+    audio_e4.play();
+}
+
+function play_f3(){
+    var audio_f3 = document.getElementById('note_f3');
+    audio_f3.play();
+}
+
+function play_f_3(){
+    var audio_f_3 = document.getElementById('note_f_3');
+    audio_f_3.play();
+}
+
+function play_f4(){
+    var audio_f4 = document.getElementById('note_f4');
+    audio_f4.play();
+}
+
+function play_f_4(){
+    var audio_f_4 = document.getElementById('note_f_4');
+    audio_f_4.play();
+}
+
+function play_g3(){
+    var audio_g3 = document.getElementById('note_g3');
+    audio_g3.play();
+}
+
+function play_g_3(){
+    var audio_g_3 = document.getElementById('note_g_3');
+    audio_g_3.play();
+}
+
+function play_g4(){
+    var audio_g4 = document.getElementById('note_g4');
+    audio_g4.play();
+}
+
+function play_g_4(){
+    var audio_g_4 = document.getElementById('note_g_4');
+    audio_g_4.play();
+}
